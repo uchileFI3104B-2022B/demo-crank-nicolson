35a0c63552521ddb4f15025bfd34b2c27398da08
Valentino Gonzalez <valentino.g.gonzalez@gmail.com>
2020-11-01 16:17:50 -0300

    Agrega discrtizacion del espacio y grafica condicion inicial

--- c-n.py
+++ c-n.py
@@ -8,3 +8,25 @@ T(t, x=0) = T(t, x=1) = 0
 y 
 T(t=0, x) = sin(pi x)
 """
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Discretizacion del espacio
+N = 5  # numero de puntos a evaluar en el espacio
+h = 1 / (N-1)  # delta x
+
+x = np.linspace(0, 1, N)
+
+
+# Visualizacion de la condicion inicial
+plt.figure(1)
+plt.clf()
+plt.plot(x, np.sin(np.pi * x), '-', label='T(t=0, x)')
+
+plt.xlabel('x')
+plt.ylabel('T(x)')
+plt.legend()
+plt.show()
+
