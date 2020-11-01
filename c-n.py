820df67a012596e5f356403ba3e0733b3a06b9d0
Valentino Gonzalez <valentino.g.gonzalez@gmail.com>
2020-11-01 16:36:17 -0300

    Agrega definicion de matriz tridiagonal del lado izquierdo de la ec. de C-N

--- c-n.py
+++ c-n.py
@@ -11,6 +11,7 @@ T(t=0, x) = sin(pi x)
 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.sparse import diags  # to easily create diagonal matrices
 
 
 # Discretizacion del espacio
@@ -18,6 +19,21 @@ N = 5  # numero de puntos a evaluar en el espacio
 h = 1 / (N-1)  # delta x
 
 x = np.linspace(0, 1, N)
+
+
+# Definiendo matriz tridiagonal
+epsilon = 0.1  # delta t adimensionalizado, partimos con numero arbitrario
+s = epsilon / 2 / h**2
+
+# shape de la matriz
+shape = (N-2, N-2)
+diagonal = np.ones(N-2) * (2*s + 1)
+off_diagonals = np.ones(N-3) * (-s)
+S = diags([off_diagonals, diagonal, off_diagonals], offsets=[1, 0, -1])
+
+# Para visualizar la matriz S se puede hacer:
+# S.toarray()
+# pero conviene mantenerla como una "sparse matrix" para eficiencia
 
 
 # Visualizacion de la condicion inicial
